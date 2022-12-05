4dbcee4d86cbe4aae0a0335fa688b9373a596882
zir31 <redstorm31rus@gmail.com>
2022-12-05 19:22:19 +0300

    Added Automapper

---
+++ TraineeHelper.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace TraineeHelper.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+}
+}
+
