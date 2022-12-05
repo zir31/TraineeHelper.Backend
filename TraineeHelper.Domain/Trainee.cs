b721d9dcfc8084dcb065846fbd8bbe8cfc089b75
zir31 <redstorm31rus@gmail.com>
2022-12-05 18:05:45 +0300

    Add project files.

---
+++ TraineeHelper.Domain/Trainee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TraineeHelper.Domain
+{
+    public class Trainee
+    {
+        Guid Id { get; set; }
+        string FullName { get; set; }
+        IEnumerable<string> Skills { get; set; }
+    }
+}
